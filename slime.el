e0dc7cf9f658c9bfc9d711d78a51340ffae035ad
Attila Lendvai <attila.lendvai@gmail.com>
2010-10-07 16:17:14 +0200

    Order restarts in SLDB so that restarts earlier on the binding stack have smaller (therefore more stable) numbered key bindings assigned to them.

--- slime.el
+++ slime.el
@@ -5424,7 +5424,7 @@ Full list of commands:
     (eval `(defun ,fname ()
              ,docstring
              (interactive)
-             (sldb-invoke-restart ,number)))
+             (sldb-invoke-restart (- (length sldb-restarts) ,number 1))))
     (define-key sldb-mode-map (number-to-string number) fname)))
 
 
@@ -5584,13 +5584,14 @@ RESTARTS should be a list ((NAME DESCRIPTION) ...)."
                (cl-loop for (name nil) in (cl-subseq restarts start end)
                         maximize (length name))
              for (name string) in (cl-subseq restarts start end)
-             for number from start
+             for number from start  
+             for i downfrom (- len start 1)
              do (slime-insert-propertized
                  `(,@nil restart ,number
                          sldb-default-action sldb-invoke-restart
                          mouse-face highlight)
-                 " " (in-sldb-face restart-number (number-to-string number))
-                 ": ["  (in-sldb-face restart-type name) "] "
+                 " " (in-sldb-face restart-number (number-to-string i))
+                 ": "  (in-sldb-face restart-type name) " "
                  (make-string (- longest-restart-name-length
                                  (length name))
                               ?\ )
